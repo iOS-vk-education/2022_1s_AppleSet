af933677a0876ee7ed586169b4e6c4c6cb7599d1
pechenovadasha <pechenkadasha250500@gmail.com>
2022-11-21 22:10:56 +0300

    Добавлен переход к дивайсам, но не отображается картинка

--- ITS/ITS/Controllers/PlaceViewController.swift
+++ ITS/ITS/Controllers/PlaceViewController.swift
@@ -6,14 +6,55 @@
 //
 
 import UIKit
+import PinLayout
 
 final class PlaceViewController: UIViewController {
 
+    private let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumLineSpacing = 8
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(DeviceCell.self, forCellWithReuseIdentifier: "DeviceCell")
+       
+//        collectionView.contentInset = UIEdgeInsets(top: 8,
+//                                                   left: .zero,
+//                                                   bottom: .zero,
+//                                                   right: .zero)
+        
+        return collectionView
+    }()
+    
+    private var models: [DeviceCellModel] = []
+
+    private func setup()
+    {
+        view.backgroundColor = .white
+        
+        collectionView.backgroundColor = .white
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        view.addSubview(collectionView)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .white
+        setup()
+        loadDevice()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
         
+        collectionView.pin
+            .top(view.safeAreaInsets.top)
+            .horizontally()
+            .bottom()
+    }
+    
+    private func loadDevice() {
+        let deviceModel = DeviceCellDataModel()
+        models = deviceModel.loadPlaces()
     }
     
     // MARK: - viewWillAppear
@@ -43,3 +84,34 @@ final class PlaceViewController: UIViewController {
         dismiss(animated: true)
     }
 }
+
+extension PlaceViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return models.count
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "DeviceCell",
+                                                          for: indexPath) as? DeviceCell,
+                models.count > indexPath.row
+            else {
+                return UICollectionViewCell()
+        }
+        
+        cell.configure(with: models[indexPath.row])
+        return cell
+    }
+}
+
+extension PlaceViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        if indexPath.row == 0 {
+            return CGSize(width: view.frame.width, height: 100)
+        } else {
+            return CGSize(width: view.frame.width, height: view.frame.width)
+        }
+    }
+}
+
--- ITS/ITS/Models/DeviceCellModel.swift
+++ ITS/ITS/Models/DeviceCellModel.swift
@@ -2,7 +2,21 @@
 //  DeviceCellModel.swift
 //  ITS
 //
-//  Created by Natalia on 20.11.2022.
+//  Created by Dasha on 20.11.2022.
 //
 
-import Foundation
+import UIKit
+// Данные приходят с бэка
+final class DeviceCellModel {
+    let name: String
+    let image: UIImage?
+    let values: String
+    
+    // Инициализация ячейки
+    init(name: String = "", values: String = "", image: UIImage? = nil ) {
+        self.name = name
+        self.image = image
+        self.values = values
+    }
+}
+
